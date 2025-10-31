b99ffbd637de3b64faeb9b8f1e8f5bd25025cea8
Nidhi <ojhanidhi036@gmail.com>
2025-10-31 23:43:42 +0530

    sortZeroesAndOnes, sortArrayByParity

---
+++ Array/single_multi_Array/Pointers_lect5/SortArray.java
@@ -0,0 +1,117 @@
+package Array.single_multi_Array.Pointers_lect5;
+
+import java.util.Scanner;
+
+public class SortArray {
+
+     static void sortArrayByParity(int[] arr)
+     {
+        int n = arr.length;
+        int left=0, right = n-1;
+
+        while (left < right) {
+            if(arr[left]%2==1 && arr[right]%2==0)
+            {
+                swapInArray(arr, left, right);
+                left++;
+                right--;
+            }
+            if(arr[left]%2 == 0){
+                left++;
+            }
+            if(arr[right]%2 == 1){
+                right--;
+            }
+        }
+        printArray(arr);
+
+     }
+
+     static void sortZeroesAndOnesPointers(int[] arr)
+     {
+        int n = arr.length;
+        int left=0, right = n-1;
+
+        while (left < right) {
+            if(arr[left]==1 && arr[right]==0)
+            {
+                swapInArray(arr, left, right);
+                left++;
+                right--;
+            }
+            if(arr[left] == 0){
+                left++;
+            }
+            if(arr[right] == 1){
+                right--;
+            }
+        }
+        printArray(arr);
+
+     }
+
+    static void sortZeroesAndOnes(int[] arr)
+    {
+        int n = arr.length;
+        int zeroes = 0;
+        for(int i=0; i<n; i++)
+        {
+            if(arr[i] == 0)
+            {
+                zeroes++;
+            }
+        }
+        // 0 to zeroes-1 : 0, zeroes to n-1 : 1
+        for(int i=0; i<n; i++)
+        {
+            if(i<zeroes)
+            {
+                arr[i] = 0;
+            }else{
+                arr[i] = 1;
+            }
+        }
+    }
+
+    static void swapInArray(int[] arr, int i, int j)
+    {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    static void printArray(int[] arr)
+    {
+        for(int i=0; i<arr.length; i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter "+n+" element");
+        for(int i=0; i<arr.length; i++)
+        {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Original Array");
+        printArray(arr);
+        // sortZeroesAndOnes(arr);
+        // System.out.println("Sorted Array");
+        // printArray(arr);
+         
+        // System.out.println("Sorted Array Using Pointers");
+        // sortZeroesAndOnesPointers(arr);
+
+        System.out.println("Sorted Array By Parity");
+        sortArrayByParity(arr);
+       
+
+    }
+    
+}
