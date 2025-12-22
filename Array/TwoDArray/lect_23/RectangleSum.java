190f62e9fbf863722aecd3708017060c2d71fe9d
Nidhi <ojhanidhi036@gmail.com>
2025-12-22 17:24:28 +0530

    Pascal triangle: prefix(without loop)

--- Array/TwoDArray/lect_23/RectangleSum.java
+++ Array/TwoDArray/lect_23/RectangleSum.java
@@ -7,6 +7,26 @@ import java.util.Scanner;
 
 public class RectangleSum {
 
+    static int findSum3(int[][] matrix, int l1, int r1, int l2, int r2)
+    {
+        int ans=0;
+        findPrefixSumMatrix(matrix);
+        int sum=0, up=0, left=0, leftUp=0;
+
+        sum = matrix[l2][r2];
+
+        if(r1>=1)
+            left = matrix[l2][r1-1];
+        if(l1>=1)
+            up = matrix[l1-1][r2];
+        if(l1>=1 && r1>=1)
+            leftUp = matrix[l1-1][r1-1];
+
+        ans = sum-up-left+leftUp;
+        
+        return ans;
+
+    }
 
     static int findSum2(int[][] matrix, int l1, int r1, int l2, int r2)
     {
@@ -53,6 +73,15 @@ public class RectangleSum {
             }
         }
 
+        //traverse vertically to calculate columnwise prefix sum
+        for(int j=0; j<c; j++)
+        {
+            for(int i=1; i<r; i++)
+            {
+                matrix[i][j] += matrix[i-1][j];
+            }
+        }
+
         
     }
 
@@ -82,6 +111,7 @@ public class RectangleSum {
 
         System.out.println("Sum of rectangle: "+findSum(matrix, l1, r1, l2, r2));
         System.out.println("Sum of rectangle: "+findSum2(matrix, l1, r1, l2, r2));
+        System.out.println("Sum of rectangle: "+findSum3(matrix, l1, r1, l2, r2));
 
     }
 
