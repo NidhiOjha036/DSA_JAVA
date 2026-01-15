cd5d8c4cde06f0b91a5e7607a7bb358eefe60b75
Nidhi <ojhanidhi036@gmail.com>
2026-01-15 18:22:44 +0530

    ReverseStringRecursion

---
+++ Recursion/lect_34/Reverse.java
@@ -0,0 +1,20 @@
+package Recursion.lect_34;
+
+import java.util.Scanner;
+
+public class Reverse {
+
+    static String reverse(String s, int idx)
+    {
+        if(idx==s.length()) return "";
+        String smallAns = reverse(s, idx+1);
+        return smallAns + s.charAt(idx);
+    }
+
+    public static void main(String[] args) {
+        // Scanner sc = new Scanner(System.in);
+        // String s = sc.nextLine();
+        String s = "abcd";
+        System.out.println(reverse(s, 0));
+    }
+}
