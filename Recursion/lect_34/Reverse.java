578be83b66a8c6bce9cfe86df7f48bdd5f5d6835
Nidhi <ojhanidhi036@gmail.com>
2026-01-16 21:44:48 +0530

    ReverseRecursion palindrome

--- Recursion/lect_34/Reverse.java
+++ Recursion/lect_34/Reverse.java
@@ -14,7 +14,14 @@ public class Reverse {
     public static void main(String[] args) {
         // Scanner sc = new Scanner(System.in);
         // String s = sc.nextLine();
-        String s = "abcd";
-        System.out.println(reverse(s, 0));
+        String s = "aaaa";
+        //System.out.println(reverse(s, 0));
+        String rev = reverse(s, 0);
+        if(rev.equals(s))
+        {
+            System.out.printf("%s is Palindrome",s);
+        }else{
+            System.out.printf("%s is not Palindrome",s);
+        }
     }
 }
