14864771c9cd2e0e9266509f61f6a2e9f9c36706
Nidhi <ojhanidhi036@gmail.com>
2026-01-19 13:36:02 +0530

    FrogjumpRecursion

---
+++ Recursion/lect_36/keypadCombinations.java
@@ -0,0 +1,14 @@
+package Recursion.lect_36;
+
+public class keypadCombinations {
+
+    static void combination(String dig, String kp, String res)
+    {
+
+    }
+
+    public static void main(String[] args) {
+        String s = "23";
+        String[] kp = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    }
+}
