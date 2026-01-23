662ebf4f8946ed43e07b694020d453a9eaa93352
Nidhi <ojhanidhi036@gmail.com>
2026-01-23 23:34:35 +0530

    SelectionSort

---
+++ Recursion/lect_37/SelectionSort.java
@@ -0,0 +1,36 @@
+package Recursion.lect_37;
+
+public class SelectionSort {
+
+    static void selectionSort(int[] arr)
+    {
+        int n = arr.length;
+        for(int i=0; i<n-1; i++)
+        {
+            int min_ind = i;
+            for(int j=i+1; j<n; j++)
+            {
+                if(arr[j]<arr[min_ind])
+                {
+                    min_ind = j;
+                }
+            }
+
+            int temp = arr[i];
+            arr[i] = arr[min_ind];
+            arr[min_ind] = temp;
+
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {7,4,5,1,2,20};
+        selectionSort(arr);
+        for(int i: arr)
+        {
+            System.out.print(i+" ");
+        }
+        
+    }
+}
