4d7a096cd80477263006ab45b1037988aa24ab92
Nidhi <ojhanidhi036@gmail.com>
2026-03-18 11:29:08 +0530

    added recursiveBinarySearch

--- Sorting/lect_45/BinarySearch.java
+++ Sorting/lect_45/BinarySearch.java
@@ -2,6 +2,15 @@ package Sorting.lect_45;
 
 public class BinarySearch {
 
+    static boolean recursiveBinarySearch(int[] a, int st, int end, int target)
+    {
+        if(st>end) return false;
+        int mid = (st+end)/2;
+        if(target == a[mid]) return true;
+        else if(target<a[mid]) return recursiveBinarySearch(a,st,mid-1,target);
+        else return recursiveBinarySearch(a,mid+1,end,target);
+    }
+
     static boolean binarySearch(int[] a, int target)
     {
         int n = a.length;
@@ -19,12 +28,19 @@ public class BinarySearch {
     public static void main(String[] args) {
         int[] a = {1,2,3,4,5};
         int target = 0;
+
+        // while(target != 10)
+        // {
+        //     System.out.printf("%d exists in array: %b \n",target,binarySearch(a, target));
+        //     target++;
+        // }
+
         while(target != 10)
         {
-            System.out.printf("%d exists in array: %b \n",target,binarySearch(a, target));
+            System.out.printf("%d exists in array: %b \n",target,recursiveBinarySearch(a, 0, a.length-1, target));
             target++;
         }
-        
+
     }
     
 }
