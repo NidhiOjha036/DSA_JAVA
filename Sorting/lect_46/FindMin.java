9ae24a54b8d26f86f4488917d700332e0142fdb3
Nidhi <ojhanidhi036@gmail.com>
2026-03-20 13:31:33 +0530

    FindMinimumInRotatedArray

---
+++ Sorting/lect_46/FindMin.java
@@ -0,0 +1,28 @@
+package Sorting.lect_46;
+
+public class FindMin {
+
+    static int findMinimum(int[] a)
+    {
+        int n = a.length-1;
+        int st=0, end=n-1;
+        int ans=-1;
+        while(st<=end)
+        {
+            int mid = st+(end-st)/2;
+            if(a[mid]<=a[n-1])
+            {
+                ans = mid;
+                end=mid-1;
+            }else{//a[mid]>a[n-1]
+                st = mid+1;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {3,4,5,1,2};
+        System.out.println(findMinimum(a));
+    }
+}
---
+++ Sorting/lect_46/RotatedSortArray.java
@@ -0,0 +1,7 @@
+package Sorting.lect_46;
+
+public class RotatedSortArray {
+    public static void main(String[] args) {
+        
+    }
+}
