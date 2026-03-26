338ae1ffe5d36de398cef4e5d987eda3bb98f62f
Nidhi <ojhanidhi036@gmail.com>
2026-03-26 19:23:09 +0530

    StringBuilder

---
+++ String/Builder.java
@@ -0,0 +1,17 @@
+package String;
+
+public class Builder {
+    public static void main(String[] args) {
+
+        StringBuilder str = new StringBuilder("hello");
+        System.out.println(str); // hello
+        str.setCharAt(0, 'm');
+        System.out.println(str); //mello
+        str.append(10);
+        System.out.println(str); //mello10
+        str.insert(2, 'y');
+        System.out.println(str); //meyllo10
+
+    }
+    
+}
