ae621cc323a1aafc6854543d3ebace0658eca591
Nidhi <ojhanidhi036@gmail.com>
2026-03-27 10:11:41 +0530

    PalindromicSubstring

---
+++ String/PalindromicSubstring.java
@@ -0,0 +1,31 @@
+package String;
+
+public class PalindromicSubstring {
+
+    static boolean isPalindrome(String s)
+    {
+        int i=0,j=s.length()-1;
+        while (i<j) {
+            if(s.charAt(i) != s.charAt(j)) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String s = "abbd";
+        int count=0;
+        for(int i=0; i<s.length(); i++)
+        {
+            for(int j=i+1; j<=s.length(); j++)
+            {
+                if(isPalindrome(s.substring(i,j))){
+                    System.out.print(s.substring(i,j)+" ");
+                    count++;
+                }
+            }
+        }
+        System.out.println("\n Number of Palindrome Substring: "+count);
+    }
+}
