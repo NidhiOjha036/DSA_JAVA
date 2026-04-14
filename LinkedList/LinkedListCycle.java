e11299c72af920e61ee49cbac13a421e9bc76319
Nidhi <ojhanidhi036@gmail.com>
2026-04-14 11:30:29 +0530

    LinkedListCycleDetected

---
+++ LinkedList/LinkedListCycle.java
@@ -0,0 +1,55 @@
+package LinkedList;
+
+public class LinkedListCycle {
+
+    public static class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) {
+          val = x;
+          next = null;
+      }
+    }
+
+   public static ListNode detectCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        // Step 1: Detect cycle safely
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                // Step 2: Find cycle start
+                ListNode temp = head;
+                while (temp != slow) {
+                    temp = temp.next;
+                    slow = slow.next;
+                }
+                return temp;
+            }
+        }
+
+        return null; // No cycle
+    }
+    public static void main(String[] args) {
+
+        ListNode a = new ListNode(1);
+        ListNode b = new ListNode(2);
+        ListNode c = new ListNode(3);
+        ListNode d = new ListNode(4);
+        ListNode e = new ListNode(5);
+        ListNode f = new ListNode(6);
+
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+        e.next = f;  //[1,2,3,4,5,6]
+        f.next = c;
+
+        ListNode cycleNode = detectCycle(a);
+        System.out.println("CycleNode: "+cycleNode.val);
+    }
+}
