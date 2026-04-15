0165ec28701ac4f8c183530a4a5f0e20ad0f1cf3
Nidhi <ojhanidhi036@gmail.com>
2026-04-15 15:22:11 +0530

    palindrome corrected

--- LinkedList/ReverseList.java
+++ LinkedList/ReverseList.java
@@ -33,6 +33,62 @@ public class ReverseList {
         }
         return prev;
     }
+
+    public static boolean isPalin(Node head)
+    {
+        Node slow = head;
+        Node fast = head;
+        while(fast.next != null && fast.next.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        Node temp = reverseIterative(slow.next);
+        slow.next = temp;
+        Node p1 = head;
+        Node p2 = slow.next;
+        while(p2!=null)
+        {
+            if(p1.val != p2.val) return false;
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+        return true;
+    }
+
+        public static boolean isPalindrome(Node head) {
+        // Step 1: create deep copy
+        Node copy = null, tail = null;
+        Node temp = head;
+
+        while (temp != null) {
+            Node newNode = new Node(temp.val);
+            if (copy == null) {
+                copy = newNode;
+                tail = newNode;
+            } else {
+                tail.next = newNode;
+                tail = newNode;
+            }
+            temp = temp.next;
+        }
+
+        // Step 2: reverse the copy
+        Node rHead = reverseIterative(copy);
+
+        // Step 3: compare original and reversed copy
+        Node oHead = head;
+        while (oHead != null && rHead != null) {
+            if (oHead.val != rHead.val) {
+                return false;
+            }
+            oHead = oHead.next;
+            rHead = rHead.next;
+        }
+
+        return true;
+    }
+
     static void display(Node head)
     {
         if(head == null) 
@@ -54,9 +110,12 @@ public class ReverseList {
         b.next = c;
         c.next = d;
         d.next = e;
+        // display(a);
+        // //a = reverse(a);
+        // a = reverseIterative(a);
         display(a);
-        //a = reverse(a);
-        a = reverseIterative(a);
-        display(a);
+       // System.out.println("IsPalinddrome: "+isPalindrome(a));
+       System.out.println("isPalin: "+isPalin(a));
+        
     }
 }
