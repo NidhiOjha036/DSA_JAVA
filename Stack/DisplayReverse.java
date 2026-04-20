27a1516db1c7dc515ab625d2fcdd5529e5a54c7a
Nidhi <ojhanidhi036@gmail.com>
2026-04-20 18:09:45 +0530

    popBottom and pushBottom

--- Stack/DisplayReverse.java
+++ Stack/DisplayReverse.java
@@ -3,6 +3,7 @@ package Stack;
 import java.util.Stack;
 
 public class DisplayReverse {
+
     static void displayRecReverse(Stack<Integer> st)
     {
         if(st.size() == 0) return;
@@ -19,6 +20,32 @@ public class DisplayReverse {
         System.out.print(top+" ");
         st.push(top);
     }
+
+    static void pushBottom(Stack<Integer> st,int val)
+    {
+        if(st.size() == 0){
+            st.push(val);
+            System.out.print(val+" ");
+            return;
+        }
+        int top = st.pop();
+        pushBottom(st, val);
+        System.out.print(top+" ");
+        st.push(top);
+    }
+
+    static void popBottom(Stack<Integer> st)
+    {
+        if(st.size() == 1){
+            st.pop();
+            return;
+        }
+        int top = st.pop();
+        popBottom(st);
+        System.out.print(top+" ");
+        st.push(top);
+    }
+
     public static void main(String[] args) {
         Stack<Integer> st = new Stack<>();
         st.push(1);
@@ -29,7 +56,9 @@ public class DisplayReverse {
         System.out.println(st);
         displayRec(st);
         System.out.println();
-        displayRecReverse(st);
-        
+        // displayRecReverse(st);
+       // pushBottom(st, 6);
+
+        popBottom(st);
     }
 }
