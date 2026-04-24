4b5c7ea5a254dab9b8652073f25fddf500de1415
Nidhi <ojhanidhi036@gmail.com>
2026-04-24 14:03:35 +0530

    nextGreaterElement

---
+++ Stack/NextGreaterElement.java
@@ -0,0 +1,56 @@
+package Stack;
+
+import java.util.Stack;
+
+public class NextGreaterElement {
+
+    static int[] nextGreaterEle(int[] arr)
+    {
+        int n = arr.length;
+        Stack<Integer> st = new Stack<>();
+        int[] res = new int[arr.length];
+        res[n-1] = -1;
+        st.push(arr[n-1]);
+        for(int i=n-2; i>=0; i--){
+            while(st.size() > 0 && st.peek() < arr[i])
+            {
+                st.pop();
+            }
+            if(st.size()==0) res[i] = -1;
+            else res[i] = st.peek();
+            st.push(arr[i]);
+        }
+        return res;
+    }
+    public static void main(String[] args) {
+
+        int[] arr = {1,3,2,1,8,6,3,4};
+        //int[] res = new int[arr.length];
+        int[] res = nextGreaterEle(arr);
+
+
+        // for(int i=0; i<arr.length; i++)
+        // {
+        //     res[i]=-1;
+        //     for(int j=i+1; j<arr.length; j++)
+        //     {
+        //         if(arr[j]>arr[i])
+        //         {
+        //             res[i] = arr[j];
+        //             break;
+        //         }
+        //     }
+        // }
+
+        for(int i=0; i<arr.length; i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+        for(int j=0; j<res.length; j++)
+        {
+            System.out.print(res[j]+" ");
+        }
+
+    }
+}
