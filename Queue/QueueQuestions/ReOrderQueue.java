2ec74fd88a39777087f02e6aa19eda5f6a465dd9
Nidhi <ojhanidhi036@gmail.com>
2026-05-29 13:44:03 +0530

    ReorderQueue

---
+++ Queue/QueueQuestions/ReOrderQueue.java
@@ -0,0 +1,60 @@
+package Queue.QueueQuestions;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class ReOrderQueue {
+    public static void main(String[] args) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+        q.add(6);
+        q.add(7);
+        q.add(8);
+        System.out.println(q);
+
+        int n = q.size();
+        Stack<Integer> st = new Stack<>();
+
+        for(int i=1; i<=n/2; i++)
+        {
+            st.push(q.remove()); //1 2 3 4
+        }
+
+        while(st.size()>0)
+        {
+            q.add(st.pop());   //5 6 7 8 4 3 2 1
+        }
+
+        // queue 4 3 2 1 
+        // stack 5 6 7 8
+        for(int i=1; i<=n/2; i++)
+        {
+            st.push(q.remove()); //5 6 7 8
+        }
+
+        for(int i=1; i<=n/2; i++)
+        {
+            q.add(st.pop());
+            q.add(q.remove());  //8 4 7 3 6 2 5 1
+        }
+
+        //Assign into stack (reverse will get)
+        while(q.size() > 0)
+        {
+            st.push(q.remove()); //8 4 7 3 6 2 5 1
+        }
+
+        while(st.size() > 0)
+        {
+            q.add(st.pop());  //1 5 2 6 3 7 4 8
+        }
+
+        System.out.println(q);
+
+    }
+}
